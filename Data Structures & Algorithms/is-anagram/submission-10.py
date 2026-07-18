6ac4be03339b627470194747766108b75d4fec15
X_XSilver <ahmerz2013@gmail.com>
2026-07-18 05:53:01 -0500

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        chars = [0] * 26
+
+        for i in range(len(s)):
+            
+            chars [ord(s[i]) - ord('a')] += 1
+            chars [ord(t[i]) - ord('a')] -= 1
+            
+        for n in chars:
+            if n != 0:
+                return False
+        
+        return True 
